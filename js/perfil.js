caae8b5481f844963680b6f8d6c3b917613645fd
SoraiaOliveira <soraia.oliveira@ua.pt>
2026-01-10 12:50:56 +0000

    js do perfil/editarperfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,9 +1,45 @@
-var elNome = document.getElementById("nomeUtilizador")
-var elUsername = document.getElementById("username")
-var elBio = document.getElementById("bio")
+window.addEventListener("DOMContentLoaded", function () {
+    var elNome = document.getElementById("nomeUtilizador");
+    var elUsername = document.getElementById("username");
+    var elBio = document.getElementById("biografia");
 
-if (elNome && elUsername && elBio) {
-    elNome.textContent = perfil.nome + " " + perfil.apelido
-    elUsername.textContent = "@" + perfil.username
-    elBio.textContent = perfil.bio
-}
+    if (!elNome || !elUsername || elBio) return;
+
+    var userLogado = null;
+    try{
+        userLogado = JSON.parse(localStorage.getItem("userLogado"))
+    } catch (e) {
+        userLogado = null
+    }
+
+    if (!userLogado || (!userLogado.username && !userLogado.login)) {
+        window.location.href = "login.html"
+        return
+    }
+
+    var loginAtual = userLogado.login || userLogado.username;
+    var utilizadoresLS = JSON.parse(localStorage.getItem("utilizadores")) || [];
+
+    var utilizadoresFixos = [
+        { login: 'sara', pass: 'sara123', nome: 'Sara', apelido: 'Martins', avatar: null, bio: '' },
+        { login: 'joana25', pass: 'joaninha2026', nome: 'Joana', apelido: '', avatar: null, bio: '' },
+        { login: 'admin', pass: 'admin', nome: 'Admin', apelido: '', avatar: null, bio: '' }
+    ];
+
+    var utilizadores = utilizadoresFixos.concat (utilizadoresLS);
+
+    var u = null
+
+    for (var i = 0 ; i < utilizadores.length; i++) {
+        if (utilizadores[i].login === loginAtual) {
+            u = utilizadores[i]
+            break;
+        }
+    }
+
+    var nomeCompleto = u.nome + " " + u.apelido;
+    elNome.textContent = nomeCompleto
+    elUsername.textContent = "@" + u.login
+    elBio.textContent = u.bio
+
+})
